4d4657b4ba5f07fe1ccc65bba654839bb6facf38
Basil Gray <basil.s.gray@gmail.com>
2024-09-29 16:48:11 +0900

    Changed ID to varchar8, and added index for long_url

--- db/migrations.sql
+++ db/migrations.sql
@@ -1,5 +1,9 @@
 -- 28 Sep 2024
 CREATE TABLE IF NOT EXISTS urls (
-    id TEXT PRIMARY KEY,
-    long_url TEXT NOT NULL
+    id VARCHAR(8) PRIMARY KEY,
+    long_url TEXT NOT NULL UNIQUE
+);
+
+CREATE INDEX IF NOT EXISTS long_Url ON urls (
+    long_url
 );
